4975c858ce439d3978bcda39a6a40be3934388fb
Avinash <avinashkmg1310@gmail.com>
2024-11-21 23:01:34 +0530

    del

--- action/actions.jsx
+++ action/actions.jsx
@@ -1,2 +1,5 @@
 // put request to change the status of the todo to completed
 export const updateTodo = (id) => async (dispatch) => {};
+
+// delete request to delete the todo
+export const deleteTodo = (id) => async (dispatch) => {};
